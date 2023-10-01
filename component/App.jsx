0712a329a12fefd4bdfbd1ff41343188f43bb59f
Pushc <94675712+Pushc@users.noreply.github.com>
2023-10-01 23:11:46 +0530

    Initial commit

---
+++ component/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Heading from "./Heading";
+import Footer from "./Footer";
+import Notes from "./Notes";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Notes />
+      <Footer />
+    </div>
+  );
+}
+export default App;
---
+++ component/Notes.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Notes() {
+  return (
+    <div className="note">
+      <h1>Defination</h1>
+      <p>hello welcome</p>
+    </div>
+  );
+}
+export default Notes;
